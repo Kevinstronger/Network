182221dcbcba456309a164beccffacebe76d3340
Kevinstronger <zhang1830@gmail.com>
2014-02-21 09:56:22 +0800

    close HTTPConnection in finally block

---
+++ httplib-utils.py
@@ -0,0 +1,33 @@
+import json
+import httplib
+
+# json headers for gentoostats-cli
+headers = {'Accept': 'application/json'}
+
+def GET(server, url, headers, https=True):
+    """
+    Get url from server using headers 
+    """
+    if https:
+        conn = httplib.HTTPSConnection(server)
+    else:
+        conn = httplib.HTTPConnection(server)
+    try:
+        conn.request('GET', url=url, headers=headers)
+        data = conn.getresponse().read()
+    except httplib.HTTPException:
+        return None
+    finally:
+        if conn:
+            conn.close()
+    return data
+
+def deserialize(object):
+    """
+    Decode json object
+    """
+    try:
+        decoded = json.JSONDecoder().decode(object)
+    except (ValueError, TypeError):
+        return None
+    return decoded
